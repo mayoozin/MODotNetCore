6bbd45dc0fcb3ba3ec42a63d2f0255513922dd98
mayoo <mayoozin.developer@gmail.com>
2024-04-11 20:47:48 +0630

    [ Update ] Blog

--- MODotNetCore.ConsoleApp/AdoDotNetExample.cs
+++ MODotNetCore.ConsoleApp/AdoDotNetExample.cs
@@ -72,5 +72,32 @@ This day commemorates a significant milestone in human history:";
             string message = result > 0 ? "Saving Successful" : "Saving Failed";
             Console.WriteLine(message);
         }
+
+        public void Update()
+        {
+            string blogId = "1";
+            string title = "International Day of Human Space";
+            string author = "David Mamama";
+            string content = @"12th April marks the International Day for Human Space Flight, observed worldwide. 
+This day commemorates a significant milestone in human history:";
+            SqlConnection con = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+            con.Open();
+            Console.WriteLine("Connection Open");
+            string query = @"UPDATE [dbo].[Tbl_Blog]
+   SET [BlogTitle] = @BlogTitle
+      ,[BlogAuthor] = @BlogAuthor
+      ,[BlogContent] = @BlogContent
+ WHERE BlogId=@BlogId";
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@BlogId", blogId);
+            cmd.Parameters.AddWithValue("@BlogTitle", title);
+            cmd.Parameters.AddWithValue("@BlogAuthor", author);
+            cmd.Parameters.AddWithValue("@BlogContent", content);
+            int result = cmd.ExecuteNonQuery();
+
+            con.Close();
+            string message = result > 0 ? "Saving Successful" : "Saving Failed";
+            Console.WriteLine(message);
+        }
     }
 }
