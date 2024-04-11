6530031239c2c6c31cd08254cf7f22c5bf23e816
mayoo <mayoozin.developer@gmail.com>
2024-04-11 21:12:38 +0630

    [ Added ] Common Query File

---
+++ MODotNetCore.ConsoleApp/CommonQuery.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MODotNetCore.ConsoleApp
+{
+    internal class CommonQuery
+    {
+        public static string SelectQuery = "Select * From Tbl_Blog";
+
+        public static string CreateQuery = @"INSERT INTO [dbo].[Tbl_Blog]
+           ([BlogTitle]
+           ,[BlogAuthor]
+           ,[BlogContent])
+     VALUES
+                    (@BlogTitle,
+                    @BlogAuthor,
+                    @BlogContent)";
+
+        public static string UpdateQuery = @"UPDATE [dbo].[Tbl_Blog]
+   SET [BlogTitle] = @BlogTitle
+      ,[BlogAuthor] = @BlogAuthor
+      ,[BlogContent] = @BlogContent
+ WHERE BlogId=@BlogId";
+
+        public static string DeleteQuery = @"DELETE FROM [dbo].[Tbl_Blog]
+      WHERE BlogId=@BlogId";
+
+        public static string GetDataById = @"SELECT * FROM [dbo].[Tbl_Blog]
+      WHERE BlogId=@blogId";
+    }
+}
