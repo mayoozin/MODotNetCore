93c14df50866cbbfa06890a706ef88a52da5e829
mayoo <mayoozin.developer@gmail.com>
2024-04-11 20:08:37 +0630

    - [ Add AdoDotNetExample ]

---
+++ MODotNetCore.ConsoleApp/AdoDotNetExample.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MODotNetCore.ConsoleApp
+{
+    internal class AdoDotNetExample
+    {
+        public void Read()
+        {
+            SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
+            stringBuilder.DataSource = "DESKTOP-QIPPQBI\\SQLEXPRESS";
+            stringBuilder.InitialCatalog = "DotNetTrainingBatch4";
+            stringBuilder.UserID = "sa";
+            stringBuilder.Password = "sasa@123";
+            SqlConnection con = new SqlConnection(stringBuilder.ConnectionString);
+            con.Open();
+            Console.WriteLine("Connection Open");
+
+            #region Read
+
+            string query = "Select * From Tbl_Blog";
+            SqlCommand cmd = new SqlCommand(query, con);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+
+            #endregion
+
+            con.Close();
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                Console.WriteLine("Blog Id => " + dr["BlogId"]);
+                Console.WriteLine("Blog Title => " + dr["BlogTitle"]);
+                Console.WriteLine("Blog Author => " + dr["BlogAuthor"]);
+                Console.WriteLine("Blog Content => " + dr["BlogContent"]);
+            }
+        }
+    }
+}
--- MODotNetCore.ConsoleApp/Program.cs
+++ MODotNetCore.ConsoleApp/Program.cs
@@ -1,27 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using MODotNetCore.ConsoleApp;
 using System.Data;
 using System.Data.SqlClient;
 
 Console.WriteLine("Hello, World!");
 
-SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
-stringBuilder.DataSource = "DESKTOP-QIPPQBI\\SQLEXPRESS";
-stringBuilder.InitialCatalog = "DotNetTrainingBatch4";
-stringBuilder.UserID = "sa";
-stringBuilder.Password = "sasa@123";
-SqlConnection con = new SqlConnection(stringBuilder.ConnectionString);
-con.Open();
-Console.WriteLine("Connection Open");
+AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+adoDotNetExample.Read();
 
-#region Read
-
-string query = "Select * From Tbl_Blog";
-SqlCommand cmd = new SqlCommand(query, con);
-SqlDataAdapter adapter = new SqlDataAdapter(cmd);
-DataTable dt = new DataTable();
-adapter.Fill(dt);
-
-#endregion
-
-
-con.Close();
+Console.ReadLine();
