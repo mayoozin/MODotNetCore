8e74033afdd513d4fe7c20b431614258d32ced85
mayoo <mayoozin.developer@gmail.com>
2024-04-16 00:02:37 +0630

    - [ Added] User Model

---
+++ MODotNetCore.ConsoleApp/Model/UserModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MODotNetCore.ConsoleApp.Model
+{
+    public class Users
+    {
+        public string UserName;
+        public string Password;
+        private int id;
+
+        public Users(string username, string password)
+        {
+            this.UserName = username;
+            this.Password = password;
+        }
+    }
+}
