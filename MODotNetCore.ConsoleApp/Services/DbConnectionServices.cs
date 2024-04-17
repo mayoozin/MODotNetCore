245e931c5ded3ae7ba90e30219f987751c69623f
mayoo <mayoozin.developer@gmail.com>
2024-04-17 23:21:31 +0630

    - [ Added ] Db Connection Service as a common methods

---
+++ MODotNetCore.ConsoleApp/Services/DbConnectionServices.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MODotNetCore.ConsoleApp.Services
+{
+    public class DbConnectionServices
+    {
+        public string GetConnectionString()
+        {
+            SqlConnectionStringBuilder _sqlConnectionStringBuilder = new()
+            {
+                DataSource = "DESKTOP-QIPPQBI\\SQLEXPRESS",
+                InitialCatalog = "DotNetTrainingBatch4",
+                UserID = "sa",
+                Password = "sasa@123"
+            };
+            return _sqlConnectionStringBuilder.ConnectionString;
+        }
+
+    }
+}
