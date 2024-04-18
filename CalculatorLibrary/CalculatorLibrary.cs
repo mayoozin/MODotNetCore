fea56b983c07be9989d97966207fd717c217d304
mayoo <mayoozin.developer@gmail.com>
2024-04-18 21:53:10 +0630

    - [ Added ] Calculator

---
+++ CalculatorLibrary/CalculatorLibrary.cs
@@ -0,0 +1,32 @@
+﻿namespace CalculatorLibrary
+{
+    public class Calculator
+    {
+        public static double Calculation(double num1, double num2, string op)
+        {
+            double result = double.NaN;
+
+            switch (op)
+            {
+                case "a":
+                    result = num1 + num2;
+                    break;
+                case "s":
+                    result = num1 - num2;
+                    break;
+                case "m":
+                    result = num1 * num2;
+                    break;
+                case "d":
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
+    }
+}
