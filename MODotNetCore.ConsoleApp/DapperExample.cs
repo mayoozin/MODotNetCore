145eecbc431213e6a4eb2acd550583e4311e4d51
mayoo <mayoozin.developer@gmail.com>
2024-04-25 22:01:17 +0630

    -[ Added ] Dapper Service

---
+++ MODotNetCore.ConsoleApp/DapperExample.cs
@@ -0,0 +1,27 @@
+﻿using MODotNetCore.ConsoleApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MODotNetCore.ConsoleApp
+{
+    public class DapperExample
+    {
+        DbConnectionServices connectionServices = new DbConnectionServices();
+        string? _connectionString;
+        public void Run()
+        {
+            Read();
+        }
+
+        public void Read()
+        {
+            _connectionString = connectionServices.GetConnectionString();
+
+        }
+    }
+}
