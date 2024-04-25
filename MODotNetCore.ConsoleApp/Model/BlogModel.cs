b09b690afca05614832960fbd24e64c5ae8b671b
mayoo <mayoozin.developer@gmail.com>
2024-04-25 22:35:55 +0630

    - [ Modified ] BlogModel

--- MODotNetCore.ConsoleApp/Model/BlogModel.cs
+++ MODotNetCore.ConsoleApp/Model/BlogModel.cs
@@ -13,4 +13,11 @@ namespace MODotNetCore.ConsoleApp.Model
         public string BlogAuthor { get; set; }
         public string BlogContent { get; set; }
     }
+    public class BlogRequestModel
+    {
+        public string BlogId { get; set; }
+        public string BlogTitle { get; set; }
+        public string BlogAuthor { get; set; }
+        public string BlogContent { get; set; }
+    }
 }
