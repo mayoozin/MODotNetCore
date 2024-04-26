b24238bb57ce6372292c117da123677a1033c3a2
mayoo <mayoozin.developer@gmail.com>
2024-04-27 01:30:47 +0630

    - [ App ] appdbcontext

---
+++ MODotNetCore.ConsoleApp/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MODotNetCore.ConsoleApp.Model;
+using MODotNetCore.ConsoleApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MODotNetCore.ConsoleApp
+{
+    internal class AppDbContext : DbContext
+    {
+        private readonly DbConnectionServices _dbConnection;
+
+        public AppDbContext(DbConnectionServices dbConnection)
+        {
+            _dbConnection = dbConnection;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer();
+        }
+        public DbSet<BlogModel> Blog { get; set; }
+    }
+}
