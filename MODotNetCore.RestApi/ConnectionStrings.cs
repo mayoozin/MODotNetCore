9671f0aaea54ffe71d0c40fcf50dea78383b23fe
mayoo <mayoozin.developer@gmail.com>
2024-04-28 00:04:11 +0630

    - [ Added ] Connection Strings in EF Core

---
+++ MODotNetCore.RestApi/ConnectionStrings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MODotNetCore.RestApi.Services
+{
+    public class ConnectionStrings
+    {
+        public static SqlConnectionStringBuilder connection = new SqlConnectionStringBuilder()
+        {
+            DataSource = "DESKTOP-QIPPQBI\\SQLEXPRESS",
+            InitialCatalog = "DotNetTrainingBatch4",
+            UserID = "sa",
+            Password = "sasa@123",
+            TrustServerCertificate = true
+        };
+    }
+}
