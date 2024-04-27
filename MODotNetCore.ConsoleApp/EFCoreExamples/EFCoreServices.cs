88b9b2a5d5e337d194a4a95d32e0182fce8f9736
mayoo <mayoozin.developer@gmail.com>
2024-04-27 21:39:02 +0630

    -

---
+++ MODotNetCore.ConsoleApp/EFCoreExamples/EFCoreServices.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MODotNetCore.ConsoleApp.EFCoreExamples
+{
+    public class EFCoreServices
+    {
+        private readonly AppDbContext _appDbContext;
+    }
+}
