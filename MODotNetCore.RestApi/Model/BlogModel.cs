b96e97c47370e3de5378474e75ae7ed86804d7ed
mayoo <mayoozin.developer@gmail.com>
2024-04-28 00:04:47 +0630

    - [ Added ] Blog Model in EF Core

---
+++ MODotNetCore.RestApi/Model/BlogModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MODotNetCore.RestApi.Model
+{
+    [Table("Tbl_Blog")]
+    public class BlogModel
+    {
+        [Key]
+        public int BlogId { get; set; }
+        public string BlogTitle { get; set; }
+        public string BlogAuthor { get; set; }
+        public string BlogContent { get; set; }
+    }
+
+    public class BlogRequestModel
+    {
+        public int BlogId { get; set; }
+        public string BlogTitle { get; set; }
+        public string BlogAuthor { get; set; }
+        public string BlogContent { get; set; }
+    }
+}
