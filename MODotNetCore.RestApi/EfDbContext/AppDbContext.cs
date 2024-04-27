8b10a32853954f7630d98ed6ba27c982ddbdd728
mayoo <mayoozin.developer@gmail.com>
2024-04-28 00:03:28 +0630

    -  [ Added ] Ef Core DataContext

---
+++ MODotNetCore.RestApi/EfDbContext/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MODotNetCore.RestApi.Model;
+using MODotNetCore.RestApi.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MODotNetCore.RestApi.EfDbContext
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionStrings.connection.ConnectionString);
+        }
+        public DbSet<BlogModel> Blogs { get; set; }
+    }
+}
