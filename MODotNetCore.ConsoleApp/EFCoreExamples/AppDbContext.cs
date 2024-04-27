a388fb2804d88ac27a47c969087cb4d493e87a8f
mayoo <mayoozin.developer@gmail.com>
2024-04-28 00:05:06 +0630

    - [ Modified ] AppDbContext in console app

--- MODotNetCore.ConsoleApp/EFCoreExamples/AppDbContext.cs
+++ MODotNetCore.ConsoleApp/EFCoreExamples/AppDbContext.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using MODotNetCore.ConsoleApp.Model;
 using MODotNetCore.ConsoleApp.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MODotNetCore.ConsoleApp.EFCoreExamples
 {
