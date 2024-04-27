d25f6dd4bb5afac3c1d3219c3842b0e817cd035b
mayoo <mayoozin.developer@gmail.com>
2024-04-27 22:28:57 +0630

    -[ Added ] new connection strings

---
+++ MODotNetCore.ConsoleApp/Services/ConnectionStringServices.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MODotNetCore.ConsoleApp.Services
+{
+    internal class ConnectionStringServices
+    {
+        public static SqlConnectionStringBuilder connection = new SqlConnectionStringBuilder()
+        {
+            DataSource = "DESKTOP-QIPPQBI\\SQLEXPRESS",
+            InitialCatalog = "DotNetTrainingBatch4",
+            UserID = "sa",
+            Password = "sasa@123"
+        };
+    }
+}
