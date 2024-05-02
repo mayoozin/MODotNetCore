be639ccfbfecc70ba1b2523e8c9b3ced10163a93
mayoo <mayoozin.developer@gmail.com>
2024-05-02 22:30:10 +0630

    - [ Modified ] AdoDotNet service

--- MODotNetCore.Shared/AdoDotNetServices.cs
+++ MODotNetCore.Shared/AdoDotNetServices.cs
@@ -7,82 +7,80 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MODotNetCore.Shared
+namespace MODotNetCore.Shared;
+
+public class AdoDotNetService
 {
-    public class AdoDotNetService
+    private readonly string _connectionString;
+
+    public AdoDotNetService(string connectionString)
     {
-        private readonly string _connection;
-
-        public AdoDotNetService(string connection)
-        {
-            _connection = connection;
-        }
-        public List<T?> Query<T>(string query, object? parameters = null)
-        {
-            var list = new List<T?>();
-            using (SqlConnection con = new SqlConnection(_connection))
-            {
-                con.Open();
-                SqlCommand cmd = new SqlCommand(query, con);
-                if (parameters is not null)
-                {
-                    cmd.Parameters.AddRange(GetParameters(parameters).ToArray());
-                }
-                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
-                DataTable dataTable = new DataTable();
-                sqlDataAdapter.Fill(dataTable);
-                con.Close();
-                string json = JsonConvert.SerializeObject(dataTable);  // C to json
-                list = JsonConvert.DeserializeObject<List<T?>>(json); // Json to C
-            };
-            return list;
-        }
-        public T QueryFirstOrDefault<T>(string query, object? parameters = null)
-        {
-            var list = new List<T?>();
-            using (SqlConnection con = new SqlConnection(_connection))
-            {
-                con.Open();
-                SqlCommand cmd = new SqlCommand(query, con);
-                if (parameters is not null)
-                {
-                    cmd.Parameters.AddRange(GetParameters(parameters).ToArray());
-                }
-                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
-                DataTable dataTable = new DataTable();
-                sqlDataAdapter.Fill(dataTable);
-
-                string json = JsonConvert.SerializeObject(dataTable);  // C to json
-                list = JsonConvert.DeserializeObject<List<T?>>(json); // Json to C
-                con.Close();
-            };
-            return list[0];
-        }
-        public int Execute(string query, object? parameters = null)
-        {
-            var result = 0;
-            using (SqlConnection con = new SqlConnection(_connection))
-            {
-                con.Open();
-                SqlCommand cmd = new SqlCommand(query, con);
-                if (parameters is not null)
-                {
-                    cmd.Parameters.AddRange(GetParameters(parameters).ToArray());
-                }
-                result = cmd.ExecuteNonQuery();
-                con.Close();
-            };
-            return result;
-        }
-        public List<SqlParameter> GetParameters<T>(T? obj)
-        {
-            List<SqlParameter> parameters = new List<SqlParameter>();
-            foreach (var item in obj.GetType().GetProperties())
-            {
-                parameters.Add(new SqlParameter(item.Name, item.GetValue(obj)));
-            }
-            return parameters;
-        }
+        _connectionString = connectionString;
     }
+    public List<T?> Query<T>(string query, object? parameters = null)
+    {
+        var list = new List<T?>();
+        using (SqlConnection con = new SqlConnection(_connectionString))
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand(query, con);
+            if (parameters is not null)
+            {
+                cmd.Parameters.AddRange(GetParameters(parameters).ToArray());
+            }
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+            DataTable dataTable = new DataTable();
+            sqlDataAdapter.Fill(dataTable);
+            con.Close();
+            string json = JsonConvert.SerializeObject(dataTable);  // C to json
+            list = JsonConvert.DeserializeObject<List<T?>>(json); // Json to C
+        };
+        return list;
+    }
+    public T QueryFirstOrDefault<T>(string query, object? parameters = null)
+    {
+        var list = new List<T?>();
+        using (SqlConnection con = new SqlConnection(_connectionString))
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand(query, con);
+            if (parameters is not null)
+            {
+                cmd.Parameters.AddRange(GetParameters(parameters).ToArray());
+            }
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+            DataTable dataTable = new DataTable();
+            sqlDataAdapter.Fill(dataTable);
 
+            string json = JsonConvert.SerializeObject(dataTable);  // C to json
+            list = JsonConvert.DeserializeObject<List<T?>>(json); // Json to C
+            con.Close();
+        };
+        return list[0];
+    }
+    public int Execute(string query, object? parameters = null)
+    {
+        var result = 0;
+        using (SqlConnection con = new SqlConnection(_connectionString))
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand(query, con);
+            if (parameters is not null)
+            {
+                cmd.Parameters.AddRange(GetParameters(parameters).ToArray());
+            }
+            result = cmd.ExecuteNonQuery();
+            con.Close();
+        };
+        return result;
+    }
+    public List<SqlParameter> GetParameters<T>(T? obj)
+    {
+        List<SqlParameter> parameters = new List<SqlParameter>();
+        foreach (var item in obj.GetType().GetProperties())
+        {
+            parameters.Add(new SqlParameter(item.Name, item.GetValue(obj)));
+        }
+        return parameters;
+    }
 }
