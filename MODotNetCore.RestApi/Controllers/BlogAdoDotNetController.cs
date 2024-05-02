62870b4f41041b8dddfc08cdf33f0cf8c5c19fe8
mayoo <mayoozin.developer@gmail.com>
2024-05-02 23:30:19 +0630

    - [ Modified ] UpdatePatch using ado dot net

--- MODotNetCore.RestApi/Controllers/BlogAdoDotNetController.cs
+++ MODotNetCore.RestApi/Controllers/BlogAdoDotNetController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using MODotNetCore.ConsoleApp.Commons.Queries;
 using MODotNetCore.RestApi.Model;
@@ -16,7 +18,7 @@ namespace MODotNetCore.RestApi.Controllers
         [HttpGet]
         public IActionResult Get()
         {
-            List<BlogDataModel> list = new List<BlogDataModel>
+            List<BlogDataModel> list = new List<BlogDataModel>();
             try
             {
                 using SqlConnection _connectionString = new SqlConnection(ConnectionStrings.connection.ConnectionString);
@@ -114,5 +116,112 @@ namespace MODotNetCore.RestApi.Controllers
             }
             return Ok(message);
         }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, BlogDataModel newBlog)
+        {
+            string message = string.Empty;
+            int result = 0;
+            try
+            {
+                using SqlConnection _connectionString = new SqlConnection(ConnectionStrings.connection.ConnectionString);
+                _connectionString.Open();
+
+                string query = CommonQuery.UpdateQuery;
+                SqlCommand cmd = new SqlCommand(query, _connectionString);
+                cmd.Parameters.AddWithValue("@BlogId", newBlog.BlogId);
+                cmd.Parameters.AddWithValue("@BlogTitle", newBlog.BlogTitle);
+                cmd.Parameters.AddWithValue("@BlogAuthor", newBlog.BlogAuthor);
+                cmd.Parameters.AddWithValue("@BlogContent", newBlog.BlogContent);
+                result = cmd.ExecuteNonQuery();
+                _connectionString.Close();
+
+                message = result > 0 ? "Update Successful" : "Update Failed";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                throw;
+            }
+            return Ok(message);
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult UpdatePatch(int id, BlogModel newBlog)
+        {
+            string message = string.Empty;
+            string conditions = string.Empty;
+            string res = string.Empty;
+            var result = 0;
+            try
+            {
+                using SqlConnection _connectionString = new SqlConnection(ConnectionStrings.connection.ConnectionString);
+                _connectionString.Open();
+                string query = CommonQuery.UpdatePatch;
+                SqlCommand cmd = new SqlCommand(query, _connectionString);
+                cmd.Parameters.AddWithValue("@BlogId", newBlog.BlogId);
+                cmd.Parameters.AddWithValue("@BlogTitle", newBlog.BlogTitle);
+                cmd.Parameters.AddWithValue("@BlogAuthor", newBlog.BlogAuthor);
+                cmd.Parameters.AddWithValue("@BlogContent", newBlog.BlogContent);
+                result = cmd.ExecuteNonQuery();
+
+                var item = GetDataById(id);
+                if (!item)
+                {
+                    message = "Data Not Found!";
+                    goto Results;
+                }
+                if (!string.IsNullOrEmpty(newBlog.BlogTitle))
+                {
+                    conditions += " [BlogTitle] = @BlogTitle, ";
+                }
+                if (!string.IsNullOrEmpty(newBlog.BlogAuthor))
+                {
+                    conditions += " [BlogAuthor] = @BlogAuthor, ";
+                }
+                if (!string.IsNullOrEmpty(newBlog.BlogContent))
+                {
+                    conditions += " [BlogContent] = @BlogContent, ";
+                }
+                newBlog.BlogId = id;
+                string updateBatchQuery = $@"UPDATE [dbo].[Tbl_Blog]
+   SET {conditions}
+ WHERE BlogId = @BlogId";
+                _connectionString.Execute(updateBatchQuery, _connectionString);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                throw;
+            }
+            Results:
+            return Ok(message);
+        }
+
+        public bool GetDataById(int blogId)
+        {
+            try
+            {
+                using SqlConnection _connectionString = new SqlConnection(ConnectionStrings.connection.ConnectionString);
+
+                _connectionString.Open();
+                string query = CommonQuery.GetDataById;
+                SqlCommand cmd = new SqlCommand(query, _connectionString);
+                cmd.Parameters.AddWithValue("@blogId", blogId);
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                _connectionString.Close();
+                if (dt.Rows.Count == 0) return false;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                throw;
+            }
+
+            return true;
+        }
+
     }
 }
