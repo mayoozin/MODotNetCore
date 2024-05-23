e3078e20dd85b6d9d6b58d4f888cb72827c4c28a
mayoo <mayoozin.developer@gmail.com>
2024-05-23 09:11:32 +0630

    - Added Connection Strings

---
+++ MODotNetCore.RestApiWithNLayer/ConnectionStrings.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MODotNetCore.RestApiWithNLayer
+{
+    public class ConnectionStrings
+    {
+        public static SqlConnectionStringBuilder connection = new SqlConnectionStringBuilder()
+        {
+            DataSource = "DESKTOP-QIPPQBI\\SQLEXPRESS",
+            InitialCatalog = "DotNetTrainingBatch4",
+            UserID = "sa",
+            Password = "sasa@123",
+            TrustServerCertificate = true
+        };
+    }
+}
