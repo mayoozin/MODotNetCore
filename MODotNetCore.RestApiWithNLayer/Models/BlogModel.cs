385eb3f1f2beabcef687d85992048e2882f121d4
mayoo <mayoozin.developer@gmail.com>
2024-05-23 09:18:46 +0630

    - Modified blog model

--- MODotNetCore.RestApiWithNLayer/Models/BlogModel.cs
+++ MODotNetCore.RestApiWithNLayer/Models/BlogModel.cs
@@ -26,6 +26,12 @@ namespace MODotNetCore.RestApiWithNLayer.Models
         public string BlogContent { get; set; }
     }
 
+    public class BlogResponseModel
+    {
+        public string ErrorMessage { get; set; }
+        public string SuccessMessage { get; set; }
+    }
+
     public class BlogDataModel
     {
         public int BlogId { get; set; }
