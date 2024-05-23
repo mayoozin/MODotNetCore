c8ffbbbc5a761239477235e6cb424c7faf05635d
mayoo <mayoozin.developer@gmail.com>
2024-05-23 09:11:23 +0630

    - App db context

---
+++ MODotNetCore.RestApiWithNLayer/Db/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿namespace MODotNetCore.RestApiWithNLayer.Db
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionStrings.connection.ConnectionString);
+        }
+
+        public DbSet<BlogModel> Blogs { get; set; }
+    }
+}
