fadda7fd34b61cb18a7e5c4d6babaee7ecb0ca40
mayoo <mayoozin.developer@gmail.com>
2024-05-23 09:11:14 +0630

    - Add blogModel

---
+++ MODotNetCore.RestApiWithNLayer/Models/BlogModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MODotNetCore.RestApiWithNLayer.Models
+{
+    [Table("Tbl_Blog")]
+    public class BlogModel
+    {
+        [Key]
+        public int BlogId { get; set; }
+        public string BlogTitle { get; set; }
+        public string BlogAuthor { get; set; }
+        public string BlogContent { get; set; }
+    }
+
+    public class BlogRequestModel
+    {
+        public int BlogId { get; set; }
+        public string BlogTitle { get; set; }
+        public string BlogAuthor { get; set; }
+        public string BlogContent { get; set; }
+    }
+
+    public class BlogDataModel
+    {
+        public int BlogId { get; set; }
+        public string? BlogTitle { get; set; }
+        public string? BlogAuthor { get; set; }
+        public string? BlogContent { get; set; }
+    }
+}
