6b3f2137158c340ace5176956d58a0e0f2bb1015
mayoo <mayoozin.developer@gmail.com>
2024-05-23 09:11:04 +0630

    - List  and Created method

--- MODotNetCore.RestApiWithNLayer/Features/Blog/DA_Blog.cs
+++ MODotNetCore.RestApiWithNLayer/Features/Blog/DA_Blog.cs
@@ -1,6 +1,40 @@
-﻿namespace MODotNetCore.RestApiWithNLayer.Features.Blog
+﻿using MODotNetCore.RestApiWithNLayer.Db;
+using System.Reflection.Metadata.Ecma335;
+
+namespace MODotNetCore.RestApiWithNLayer.Features.Blog
 {
     public class DA_Blog
     {
+        private readonly AppDbContext _appDbContext;
+
+        public DA_Blog()
+        {
+            _appDbContext = new AppDbContext();
+        }
+
+        public List<BlogModel> GetBlogs()
+        {
+            var lst = _appDbContext.Blogs.ToList();
+            return lst;
+        }
+
+        public BlogModel? GetBlog(int id)
+        {
+            var item = _appDbContext.Blogs.FirstOrDefault(x => x.BlogId == id);
+            return item;
+        }
+
+        public int CreateBlog(BlogModel reqModel)
+        {
+            _appDbContext.Blogs.Add(reqModel);
+            var res = _appDbContext.SaveChanges();
+            return res;
+        }
+        public int UpdateBlog(BlogModel reqModel)
+        {
+            _appDbContext.Blogs.Add(reqModel);
+            var res = _appDbContext.SaveChanges();
+            return res;
+        }
     }
 }
