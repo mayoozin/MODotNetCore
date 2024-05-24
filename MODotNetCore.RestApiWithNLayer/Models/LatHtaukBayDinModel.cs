18825bd7ac904cc886a1d3ea93bec1812ec45898
mayoo <mayoozin.developer@gmail.com>
2024-05-24 13:36:17 +0630

    - Convert Min Thein Kha json to C#

---
+++ MODotNetCore.RestApiWithNLayer/Models/LatHtaukBayDinModel.cs
@@ -0,0 +1,23 @@
+﻿namespace MODotNetCore.RestApiWithNLayer.Models
+{
+    public class LatHtaukBayDinModel
+    {
+        public Question[] questions { get; set; }
+        public Answer[] answers { get; set; }
+        public string[] numberList { get; set; }
+    }
+
+    public class Question
+    {
+        public int questionNo { get; set; }
+        public string questionName { get; set; }
+    }
+
+    public class Answer
+    {
+        public int questionNo { get; set; }
+        public int answerNo { get; set; }
+        public string answerResult { get; set; }
+    }
+
+}
