e1e1a731d85c85d6d8b084a364838427dc1036dc
mayoo <mayoozin.developer@gmail.com>
2024-05-24 22:56:19 +0630

    - Http Client Example

---
+++ MODotNetCore.ConsoleAppHttpClientExample/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+HttpClient client = new HttpClient();
+HttpResponseMessage response = await client.GetAsync("http://localhost:5179/api/blog");
+if (response.IsSuccessStatusCode)
+{
+    string jsonStr = await response.Content.ReadAsStringAsync();
+    Console.WriteLine(jsonStr);
+}
