4a1afef9e5e32ca7293f636fa0e11b65151b336d
mayoo <mayoozin.developer@gmail.com>
2024-05-24 13:37:23 +0630

    - Convert Zodiac Json to C#

---
+++ MODotNetCore.RestApiWithNLayer/Models/ZodiacModel.cs
@@ -0,0 +1,33 @@
+﻿namespace MODotNetCore.RestApiWithNLayer.Models
+{
+    public class ZodiacModel
+    {
+        public ZodiacSignsDetail[] ZodiacSignsDetail { get; set; }
+    }
+
+    public class ZodiacSignsDetail
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string MyanmarMonth { get; set; }
+        public string ZodiacSignImageUrl { get; set; }
+        public string ZodiacSign2ImageUrl { get; set; }
+        public string Dates { get; set; }
+        public string Element { get; set; }
+        public string ElementImageUrl { get; set; }
+        public string LifePurpose { get; set; }
+        public string Loyal { get; set; }
+        public string RepresentativeFlower { get; set; }
+        public string Angry { get; set; }
+        public string Character { get; set; }
+        public string PrettyFeatures { get; set; }
+        public Trait[] Traits { get; set; }
+    }
+
+    public class Trait
+    {
+        public string name { get; set; }
+        public int percentage { get; set; }
+    }
+
+}
