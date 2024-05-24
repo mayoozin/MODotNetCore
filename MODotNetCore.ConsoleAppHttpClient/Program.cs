a26b84d70a5330a1ad7cddabfc914851659db99b
mayoo <mayoozin.developer@gmail.com>
2024-05-24 13:38:14 +0630

    - modified program using http client console

--- MODotNetCore.ConsoleAppHttpClient/Program.cs
+++ MODotNetCore.ConsoleAppHttpClient/Program.cs
@@ -1,2 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
 Console.WriteLine("Hello, World!");
+string jsonStr = await File.ReadAllTextAsync("data.json");
+var model = JsonConvert.DeserializeObject<MainDto>(jsonStr); // Json to C#
+
+Console.WriteLine(jsonStr);
+
+foreach (var item in model.questions)
+{
+    Console.WriteLine(item.questionNo);
+}
+
+Console.ReadLine();
+
+static string ToNumber(string num)
+{
+    num.Replace("၃", "3");
+    return num;
+}
+
+public class MainDto
+{
+    public Question[] questions { get; set; }
+    public Answer[] answers { get; set; }
+    public string[] numberList { get; set; }
+}
+
+public class Question
+{
+    public int questionNo { get; set; }
+    public string questionName { get; set; }
+}
+
+public class Answer
+{
+    public int questionNo { get; set; }
+    public int answerNo { get; set; }
+    public string answerResult { get; set; }
+}
+
