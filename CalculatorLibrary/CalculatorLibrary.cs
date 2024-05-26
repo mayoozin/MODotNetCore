a37b2733ee1239cfbf665859439a1dbf005fd3e1
mayoo <mayoozin.developer@gmail.com>
2024-05-26 15:37:40 +0630

    Add calcular library

--- CalculatorLibrary/CalculatorLibrary.cs
+++ CalculatorLibrary/CalculatorLibrary.cs
@@ -1,32 +1,31 @@
-﻿namespace CalculatorLibrary
-{
-    public class Calculator
-    {
-        public static double Calculation(double num1, double num2, string op)
-        {
-            double result = double.NaN;
+﻿namespace CalculatorLibrary;
 
-            switch (op)
-            {
-                case "a":
-                    result = num1 + num2;
-                    break;
-                case "s":
-                    result = num1 - num2;
-                    break;
-                case "m":
-                    result = num1 * num2;
-                    break;
-                case "d":
-                    if (num2 != 0)
-                    {
-                        result = num1 / num2;
-                    }
-                    break;
-                default:
-                    break;
-            }
-            return result;
+public class Calculator
+{
+    public static double Calculation(double num1, double num2, string op)
+    {
+        double result = double.NaN;
+
+        switch (op)
+        {
+            case "a":
+                result = num1 + num2;
+                break;
+            case "s":
+                result = num1 - num2;
+                break;
+            case "m":
+                result = num1 * num2;
+                break;
+            case "d":
+                if (num2 != 0)
+                {
+                    result = num1 / num2;
+                }
+                break;
+            default:
+                break;
         }
+        return result;
     }
 }
