0acd9e26e3f72f56d9aff16dfd124c964dfeda04
May Oo <74230123+mayoozin@users.noreply.github.com>
2024-05-27 12:57:19 +0630

    Update PizzaQuery.cs
    -

--- MODotNetCore.PizzaApi/Queries/PizzaQuery.cs
+++ MODotNetCore.PizzaApi/Queries/PizzaQuery.cs
@@ -4,12 +4,12 @@ public class PizzaQuery
 {
     public static string PizzaOrderQuery { get; } =
         @"SELECT po.*, p.Pizza, p.Price FROM [dbo].[Tbl_PizzaOrder] po
-            INNER JOIN Tbl_Pizza p on p.PizzaId = po.PizzaId
+            INNER JOIN Tbl_Pizza p ON p.PizzaId = po.PizzaId
             WHERE PizzaOrderInvoiceNo = @PizzaOrderInvoiceNo;
             ";
 
     public static string PizzaOrderDetailQuery { get; } =
         @"SELECT pod.*, pe.PizzaExtraName, pe.Price FROM [dbo].[Tbl_PizzaOrderDetail] pod
-            INNER JOIN Tbl_PizzaExtra pe on pod.PizzaExtraId = pe.PizzaExtraId
+            INNER JOIN Tbl_PizzaExtra pe ON pod.PizzaExtraId = pe.PizzaExtraId
             WHERE PizzaOrderInvoiceNo = @PizzaOrderInvoiceNo;";
 }
