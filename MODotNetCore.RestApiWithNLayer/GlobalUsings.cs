a8f9acc191b833098d6acaa5bb20677d26d1cf6a
May Oo <74230123+mayoozin@users.noreply.github.com>
2024-05-27 12:48:39 +0630

    Update GlobalUsings.cs
    add global using

--- MODotNetCore.RestApiWithNLayer/GlobalUsings.cs
+++ MODotNetCore.RestApiWithNLayer/GlobalUsings.cs
@@ -1,2 +1,5 @@
 ﻿global using Microsoft.EntityFrameworkCore;
 global using MODotNetCore.RestApiWithNLayer.Models;
+﻿global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Mvc;
+global using Newtonsoft.Json;
