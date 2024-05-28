4e5b13e993d6f4b0f4a74c5426e2e1dc01b6fbdc
mayoo <mayoozin.developer@gmail.com>
2024-05-28 23:24:36 +0630

    - Add Dev code

---
+++ MODotNetCore.ConsoleAppHttpClient/Shared/DevCode.cs
@@ -0,0 +1,10 @@
+﻿namespace MODotNetCore.ConsoleAppHttpClient.Shared;
+
+public class DevCode
+{
+    static string ToNumber(string num)
+    {
+        num.Replace("၃", "3");
+        return num;
+    }
+}
