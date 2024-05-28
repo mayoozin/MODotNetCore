cf7e574a6d84e8c13f4d82f9abae93276b4446ad
mayoo <mayoozin.developer@gmail.com>
2024-05-28 23:25:17 +0630

    - Removed data model in program.cs file

--- MODotNetCore.ConsoleAppHttpClient/Program.cs
+++ MODotNetCore.ConsoleAppHttpClient/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Newtonsoft.Json;
-using System.Text.Json.Serialization;
+using MODotNetCore.ConsoleAppHttpClient.Model;
 
 Console.WriteLine("Hello, World!");
 string jsonStr = await File.ReadAllTextAsync("data.json");
@@ -15,29 +15,5 @@ foreach (var item in model.questions)
 
 Console.ReadLine();
 
-static string ToNumber(string num)
-{
-    num.Replace("၃", "3");
-    return num;
-}
 
-public class MainDto
-{
-    public Question[] questions { get; set; }
-    public Answer[] answers { get; set; }
-    public string[] numberList { get; set; }
-}
-
-public class Question
-{
-    public int questionNo { get; set; }
-    public string questionName { get; set; }
-}
-
-public class Answer
-{
-    public int questionNo { get; set; }
-    public int answerNo { get; set; }
-    public string answerResult { get; set; }
-}
 
