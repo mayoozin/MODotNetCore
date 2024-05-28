f1259955784f3c876aef6fc19774f9a210595440
mayoo <mayoozin.developer@gmail.com>
2024-05-28 23:23:30 +0630

    - Add Data Model

---
+++ MODotNetCore.ConsoleAppHttpClient/Model/DataModel.cs
@@ -0,0 +1,21 @@
+﻿namespace MODotNetCore.ConsoleAppHttpClient.Model;
+
+public class MainDto
+{
+    public Question[] questions { get; set; }
+    public Answer[] answers { get; set; }
+    public string[] numberList { get; set; }
+}
+
+public class Question
+{
+    public int questionNo { get; set; }
+    public string questionName { get; set; }
+}
+
+public class Answer
+{
+    public int questionNo { get; set; }
+    public int answerNo { get; set; }
+    public string answerResult { get; set; }
+}
