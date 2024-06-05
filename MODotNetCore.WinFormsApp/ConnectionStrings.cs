0ace63ad68f3b942f3358d646d8a0de5b44401f0
mayoo <mayoozin.developer@gmail.com>
2024-06-05 09:39:55 +0630

    - Add connection strings

---
+++ MODotNetCore.WinFormsApp/ConnectionStrings.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.SqlClient;
+
+namespace MODotNetCore.WinFormsApp;
+
+internal static class ConnectionStrings
+{
+    public static SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+    {
+        DataSource = "DESKTOP-QIPPQBI\\SQLEXPRESS",
+        InitialCatalog = "DotNetTrainingBatch4",
+        UserID = "sa",
+        Password = "sasa@123",
+        TrustServerCertificate = true
+    };
+}
