e87ca10df6e3deb2616d021765c943b2a3b5f5eb
mayoo <mayoozin.developer@gmail.com>
2024-06-05 09:39:01 +0630

    - Form Main Menu window

---
+++ MODotNetCore.WinFormsApp/FrmMainMenu.cs
@@ -0,0 +1,22 @@
+﻿namespace MODotNetCore.WinFormsApp;
+
+public partial class FrmMainMenu : Form
+{
+    public FrmMainMenu()
+    {
+        InitializeComponent();
+    }
+
+    private void newBlogToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+        FrmBlog frm = new FrmBlog();
+        //frm.Show();
+        frm.ShowDialog();
+    }
+
+    private void blogsToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+        FrmBlogList frm = new FrmBlogList();
+        frm.ShowDialog();
+    }
+}
