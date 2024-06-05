bd8c894c9f08db0a11e89161682c0a385d0eb1fe
mayoo <mayoozin.developer@gmail.com>
2024-06-05 09:38:33 +0630

    - Enum Form control type

---
+++ MODotNetCore.WinFormsApp/Models/EnumFormControlType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MODotNetCore.WinFormsApp.Models;
+
+internal enum EnumFormControlType
+{
+    None = 0,
+    Edit = 1,
+    Delete = 2,
+    Create,
+    Confirm,
+}
