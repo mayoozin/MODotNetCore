c832f07dd86e5d355ec5032ff6cc2df8751e74e6
mayoo <mayoozin.developer@gmail.com>
2024-06-05 09:37:55 +0630

    - From Blog Widnow

---
+++ MODotNetCore.WinFormsApp/FrmBlog.cs
@@ -0,0 +1,106 @@
+using MODotNetCore.Shared;
+using System.Data.SqlClient;
+using System.Data;
+using MODotNetCore.WinFormsApp.Queries;
+using MODotNetCore.WinFormsApp.Models;
+
+namespace MODotNetCore.WinFormsApp;
+
+public partial class FrmBlog : Form
+{
+    private readonly DapperService _dapperService;
+    private readonly int _blogId;
+
+    public FrmBlog()
+    {
+        InitializeComponent();
+        _dapperService = new DapperService(ConnectionStrings.sqlConnectionStringBuilder.ConnectionString);
+    }
+
+    public FrmBlog(int blogId)
+    {
+        InitializeComponent();
+        _blogId = blogId;
+        _dapperService = new DapperService(ConnectionStrings.sqlConnectionStringBuilder.ConnectionString);
+
+        var model = _dapperService.QueryFirstOrDefault<BlogModel>("select * from tbl_blog where blogid = @BlogId",
+            new { BlogId = _blogId });
+
+        txtTitle.Text = model.BlogTitle;
+        txtAuthor.Text = model.BlogAuthor;
+        txtContent.Text = model.BlogContent;
+
+        btnSave.Visible = false;
+        btnUpdate.Visible = true;
+    }
+
+    private void btnSave_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            BlogModel blog = new BlogModel();
+            blog.BlogTitle = txtTitle.Text.Trim();
+            blog.BlogAuthor = txtAuthor.Text.Trim();
+            blog.BlogContent = txtContent.Text.Trim();
+
+            int result = _dapperService.Execute(BlogQuery.BlogCreate, blog);
+            string message = result > 0 ? "Saving Successful." : "Saving Failed.";
+            var messageBoxIcon = result > 0 ? MessageBoxIcon.Information : MessageBoxIcon.Error;
+            MessageBox.Show(message, "Blog", MessageBoxButtons.OK, messageBoxIcon);
+            if (result > 0)
+                ClearControls();
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.ToString());
+        }
+    }
+
+    private void btnCancel_Click(object sender, EventArgs e)
+    {
+        ClearControls();
+    }
+
+    private void ClearControls()
+    {
+        txtTitle.Clear();
+        txtAuthor.Clear();
+        txtContent.Clear();
+        txtTitle.Focus();
+    }
+
+    private void txtTitle_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btnUpdate_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            var item = new BlogModel
+            {
+                BlogId = _blogId,
+                BlogTitle = txtTitle.Text.Trim(),
+                BlogAuthor = txtAuthor.Text.Trim(),
+                BlogContent = txtContent.Text.Trim(),
+            };
+
+            string query = @"UPDATE [dbo].[Tbl_Blog]
+   SET [BlogTitle] = @BlogTitle
+      ,[BlogAuthor] = @BlogAuthor
+      ,[BlogContent] = @BlogContent
+ WHERE BlogId = @BlogId";
+
+            int result = _dapperService.Execute(query, item);
+            string message = result > 0 ? "Updating Successful." : "Updating Failed.";
+            MessageBox.Show(message);
+
+            Close();
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.ToString());
+        }
+    }
+}
