200b4720885325a7d6321f4ab3415ee40643c02f
mayoo <mayoozin.developer@gmail.com>
2024-06-05 09:38:43 +0630

    - Blog Query

---
+++ MODotNetCore.WinFormsApp/Queries/BlogQuery.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MODotNetCore.WinFormsApp.Queries;
+
+internal class BlogQuery
+{
+    public static string BlogCreate { get; } = @"INSERT INTO [dbo].[Tbl_Blog]
+           ([BlogTitle]
+           ,[BlogAuthor]
+           ,[BlogContent])
+     VALUES
+           (@BlogTitle
+           ,@BlogAuthor       
+           ,@BlogContent)";
+
+    public static string BlogList { get; } = @"SELECT [BlogId]
+      ,[BlogTitle]
+      ,[BlogAuthor]
+      ,[BlogContent]
+  FROM [dbo].[Tbl_Blog]";
+}
