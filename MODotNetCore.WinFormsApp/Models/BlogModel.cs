1a926ecfd758969a4f6ecf01d1546ec87c764dd6
mayoo <mayoozin.developer@gmail.com>
2024-06-05 09:38:22 +0630

    - Blog Models created

---
+++ MODotNetCore.WinFormsApp/Models/BlogModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MODotNetCore.WinFormsApp.Models;
+
+public class BlogModel
+{
+    public int BlogId { get; set; }
+    public string? BlogTitle { get; set; }
+    public string? BlogAuthor { get; set; }
+    public string? BlogContent { get; set; }
+}
